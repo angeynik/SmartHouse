68c1f47af5d28324dbd961c3af6950919d9a1b73
‘angeynik’ <angeynik@gmail.com>
2024-04-22 12:42:15 +0300

    install morgan postgres websocket edit configfile

--- srv/server.js
+++ srv/server.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const postgres = require('ps');
+const path = require('path');
 
 const app = express();
 
@@ -6,4 +10,19 @@ app.set('port', 3000);
 
  app.listen(app.get('port'), () => {
     console.log(`[OK] Server is running on localhost:${app.get('port')}`);
+ });
+
+ const WSServer = new WebSocket.Server ({port:1011}); 
+ WSServer.on ('connection', ws => {
+    ws.on ('message', message => {
+        console.log (`Message:  ${message}`); 
+    // Устанавливаем соединение
+    });
+    ws.send ('first mess'); 
+    //Отправляем сообщение
+    let i=0;
+    setInterval (()=> {
+        ws.send ('interval '+i);
+        i++;
+    }, 5000);
  });
