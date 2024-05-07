5cb065cbc3c15030f9ec03a3f894b477cab5343a
‘angeynik’ <angeynik@gmail.com>
2024-05-07 21:32:02 +0300

    Exsample 14-Slots variables  Lesson 6

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,14 @@
         <h2>Slots</h2>
         <hr />
         </div>
+        <app-slot-list>
+          <template #default="{iter, idx}"> 
+            <span style="color: #c25205"> 
+              <stong> {{ idx }}</stong>
+              Item:  {{ iter }} 
+            </span>
+          </template>
+        </app-slot-list>
         <app-block>
          <template #h3_title> 
             <h3> Новый H3 заголовок </h3> 
@@ -22,8 +30,9 @@
 
 <script>
 import AppBlock from './components/AppBlock.vue'
+import AppSlotList from './components/AppSlotList.vue'
 export default {
-  components: {AppBlock}
+  components: {AppBlock, AppSlotList}
 }
 </script>
 
---
+++ src/components/AppSlotList.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card">
+        <ul>
+            <li v-for="(item, i) in 5">
+                <slot v-bind:iter="item" :idx="i">
+
+                </slot>
+                </li>
+        </ul>
+    </div> 
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
