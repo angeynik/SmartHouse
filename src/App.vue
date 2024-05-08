8a5e219dde2300e94056908d44c73591f6b84661
‘angeynik’ <angeynik@gmail.com>
2024-05-08 10:33:55 +0300

    Exsample 16 Keep-alive  Lesson 6

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,21 @@
         <h2>Динамические и асинхронные компоненты</h2>
         <hr />
         <app-button 
-        :color="active === 'one' ? 'primary' :''" 
+        :color="oneColor" 
         @action="active='one'"
         > One  </app-button>        
         <app-button 
-        :color="active === 'two' ? 'primary' :''" 
+        :color="twoColor" 
         @action="active='two'"
          > Two  </app-button>
       </div>
       <!-- <app-text-one v-if="active === 'one'"></app-text-one>
       <app-text-two v-if="active === 'two'"></app-text-two> -->
+      <keep-alive> 
       <component :is="componentName">      </component>
+    </keep-alive>
     </div>
-      
+     
 
 </template>
 
@@ -38,7 +40,13 @@ export default {
       // }
       // return 'app-text-two'
       return 'app-text-' + this.active
-    }
+    },
+    oneColor() {
+      return this.active === 'one' ? 'primary' :''
+    },
+    twoColor() {
+      return this.active === 'two' ? 'primary' :''
+    }, 
   },
   components: {AppButton, AppTextOne, AppTextTwo}
 }
--- src/components/AppTextOne.vue
+++ src/components/AppTextOne.vue
@@ -3,6 +3,9 @@
     <h3> Это компонент кнопки One </h3>
     <hr />
     <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita, reprehenderit, culpa eum tempore dolorum impedit quo cupiditate aliquid architecto cum deserunt? Ea aut ipsam exercitationem dicta nesciunt molestias mollitia rerum. </p>
+    <div class="form-control"> 
+        <input type="text">
+    </div>
   </div>
 </template>
 
